bebdee793500eb2496c64a0433152e4583f69f65
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-11-14 16:40:27 +0100

    test zone now handling incoming URL's

--- app/screens/TestZone.js
+++ app/screens/TestZone.js
@@ -1,15 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from "react-native";
 import * as SplashScreen from "expo-splash-screen";
+import * as Linking from 'expo-linking';
 
 import Text from '../components/Text';
 
 SplashScreen.hideAsync();
 
 function TestZone(props) {
+
+    const url = Linking.useURL();
+
+    useEffect(() => {
+        if (url) {
+            const parts = Linking.parse(url);
+
+            if (parts.path !== null && parts.path !== '') {
+                alert(parts.path);
+            }
+        }
+    }, [url]);
+
+
     return (
         <View style={styles.container}>
-            <Text>Ahoy! </Text>
+            <Text>Ahoy! {url}</Text>
         </View>
     );
 }
@@ -23,3 +38,5 @@ const styles = StyleSheet.create({
 })
 
 export default TestZone;
+
+
