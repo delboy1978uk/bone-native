246cf0989afcf2b91a244ef4520b8efe865ff058
Derek Stephen McLean <delboy1978uk@gmail.com>
2022-11-23 15:22:21 +0100

    handle email token expired

--- app/api/users.js
+++ app/api/users.js
@@ -3,9 +3,11 @@ import client from './client';
 const activateAccount = (email, token, clientId, password) => client.post('/api/user/activate?XDEBUG_SESSION=PHPSTORM', {email: email, token: token, clientId: clientId, password: password});
 const getProfile = () => client.get('/api/user/profile', {});
 const register = userInfo => client.post('/api/user/register', userInfo);
+const resendactivationEmail = email => client.post('/api/user/resend-activation-email', {email: email});
 
 export default {
     activateAccount,
     getProfile,
-    register
+    register,
+    resendactivationEmail
 };
--- app/screens/ActivateUserScreen.js
+++ app/screens/ActivateUserScreen.js
@@ -2,6 +2,8 @@ import {Image, ImageBackground, Keyboard, StyleSheet, View} from "react-native";
 import React, {useState} from "react";
 import * as Yup from 'yup'
 
+import Button from '../components/Button';
+import colors from '../config/colors';
 import Text from '../components/Text';
 import ActivityIndicator from "../components/ActivityIndicator";
 import Animation from "../components/Animation";
@@ -18,8 +20,11 @@ const validationSchema = Yup.object().shape({
 
 function ActivateUserScreen({navigation, route}) {
     const activationApi = useApi(userApi.activateAccount);
+    const resendActivationApi = useApi(userApi.resendactivationEmail);
     const [accessToken, setAccessToken] = useState(null);
     const [error, setError] = useState();
+    const [mailSent, setMailSent] = useState(false);
+    const [tokenError, setTokenError] = useState();
     const {login} = useAuth();
     const email = route.params.email;
     const token = route.params.token;
@@ -31,6 +36,13 @@ function ActivateUserScreen({navigation, route}) {
 
             if (!result.ok) {
                 if (result.data) {
+                    switch (result.data.error) {
+                        case 'The email link has expired.':
+                        case 'A matching email link was not found':
+                        case 'The token did not match.':
+                            setTokenError(result.data.error)
+                            break;
+                    }
                     setError(result.data.error);
                 } else {
                     setError('An unexpected error occured');
@@ -48,11 +60,24 @@ function ActivateUserScreen({navigation, route}) {
 
     };
 
+    const resendActivationEmail = async () => {
+        resendActivationApi
+            .request(email)
+            .then(response => {
+                if (response.ok) {
+                    setMailSent(true);
+                } else {
+                    console.error(response.originalError);
+                }
+            })
+            .catch(console.error);
+    }
+
     return (
     <>
-        <ActivityIndicator visible={activationApi.loading} type={'overlay'}/>
+        <ActivityIndicator visible={activationApi.loading || resendActivationApi.loading} type={'overlay'}/>
         <ImageBackground blurRadius={10} style={styles.container} source={require('../assets/background.png')} >
-            { !accessToken &&
+            { !accessToken && !tokenError &&
                 <>
                     <Image style={styles.logo} source={require('../assets/logo.png')} />
                     <Form
@@ -83,6 +108,27 @@ function ActivateUserScreen({navigation, route}) {
                     </Form>
                 </>
             }
+            { tokenError && !mailSent &&
+                <>
+                    <Image style={styles.logo} source={require('../assets/logo.png')} />
+                    <Text style={styles.tokenError}>{tokenError}</Text>
+                    <Text style={styles.resendText}>You will need a fresh acivation email in order to continue creating your account.</Text>
+                    <Button color={'primary'} title={'Resend email'} onPress={resendActivationEmail}/>
+                </>
+            }
+            { tokenError && mailSent &&
+                <View style={styles.animationContainer}>
+                    <Animation
+                        autoPlay={true}
+                        loop={true}
+                        source={require('../assets/animations/email.json')}
+                        style={styles.animation}
+                        speed={1}
+                    />
+                    <Text style={styles.activate}>Activate your account</Text>
+                    <Text style={styles.info}>Check your email and click on the link to open the app and activate your account.</Text>
+                </View>
+            }
         </ImageBackground>
         </>
     );
@@ -99,6 +145,40 @@ const styles = StyleSheet.create({
         alignSelf: "center",
         marginTop: 50,
         marginBottom: 20,
+    },
+    resendText: {
+        color: colors.white,
+        fontSize: 17,
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    tokenError: {
+        color: colors.white,
+        fontSize: 25,
+        marginBottom: 10,
+        textAlign: 'center'
+    },
+    animationContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        paddingHorizontal: 10,
+        marginTop: -50
+    },
+    animation: {
+        width: 150,
+    },
+    activate: {
+        marginTop: 20,
+        textTransform: 'uppercase',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: colors.white
+    },
+    info: {
+        color: colors.white,
+        textAlign: 'center'
     },
 })
 
